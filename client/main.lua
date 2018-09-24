77194245f56389f6d4f0dec7aebd819b81185c17
xnubil <39658424+xnubil@users.noreply.github.com>
2018-09-24 16:08:08 -0400

    Create main.lua

---
+++ client/main.lua
@@ -0,0 +1,609 @@
+local Keys = {
+  ["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
+  ["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
+  ["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
+  ["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
+  ["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
+  ["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
+  ["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
+  ["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
+  ["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
+}
+
+local PlayerData              = {}
+local HasAlreadyEnteredMarker = false
+local LastStation             = nil
+local LastPart                = nil
+local LastPartNum             = nil
+local CurrentAction           = nil
+local CurrentActionMsg        = ''
+local CurrentActionData       = {}
+
+local hasAlreadyJoined        = false
+
+
+ESX                           = nil
+
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end
+
+	Citizen.Wait(5000)
+	PlayerData = ESX.GetPlayerData()
+end)
+
+function OpenArmoryMenu(station)
+
+
+    local elements = {
+      {label = _U('put_weapon'),     value = 'put_weapon'},
+      {label = _U('deposit_object'), value = 'put_stock'}
+    }
+
+    if PlayerData.job.grade_name == 'boss' or PlayerData.job.grade_name == 'chief' or PlayerData.job.grade_name == 'chief' then
+      table.insert(elements, {label = _U('remove_object'),  value = 'get_stock'})
+      table.insert(elements, {label = _U('get_weapon'),     value = 'get_weapon'})
+	  
+    end
+
+    ESX.UI.Menu.CloseAll()
+
+    ESX.UI.Menu.Open(
+      'default', GetCurrentResourceName(), 'armory',
+      {
+        title    = 'Evidence Locker',
+        align    = 'top-left',
+        elements = elements,
+      },
+      function(data, menu)
+
+        if data.current.value == 'get_weapon' then
+          OpenGetWeaponMenu()
+        end
+
+        if data.current.value == 'put_weapon' then
+          OpenPutWeaponMenu()
+        end
+
+        if data.current.value == 'buy_weapons' then
+          OpenBuyWeaponsMenu(station)
+        end
+
+        if data.current.value == 'put_stock' then
+          OpenPutStocksMenu()
+        end
+
+        if data.current.value == 'get_stock' then
+          OpenGetStocksMenu()
+        end
+
+      end,
+      function(data, menu)
+
+        menu.close()
+
+        CurrentAction     = 'menu_armory'
+        CurrentActionMsg  = _U('open_armory')
+        CurrentActionData = {station = station}
+      end
+    )
+
+end
+
+
+function OpenGetWeaponMenu()
+
+  ESX.TriggerServerCallback('esx_evidence:getArmoryWeapons', function(weapons)
+
+    local elements = {}
+
+    for i=1, #weapons, 1 do
+      if weapons[i].count > 0 then
+        table.insert(elements, {label = 'x' .. weapons[i].count .. ' ' .. ESX.GetWeaponLabel(weapons[i].name), value = weapons[i].name})
+      end
+    end
+
+    ESX.UI.Menu.Open(
+      'default', GetCurrentResourceName(), 'armory_get_weapon',
+      {
+        title    = 'Evidence Locker',
+        align    = 'top-left',
+        elements = elements
+      },
+      function(data, menu)
+
+        menu.close()
+
+        ESX.TriggerServerCallback('esx_evidence:removeArmoryWeapon', function()
+          OpenGetWeaponMenu()
+        end, data.current.value)
+
+      end,
+      function(data, menu)
+        menu.close()
+      end
+    )
+
+  end)
+
+end
+
+function OpenPutWeaponMenu()
+
+  local elements   = {}
+  local playerPed  = PlayerPedId()
+  local weaponList = ESX.GetWeaponList()
+
+  for i=1, #weaponList, 1 do
+
+    local weaponHash = GetHashKey(weaponList[i].name)
+
+    if HasPedGotWeapon(playerPed,  weaponHash,  false) and weaponList[i].name ~= 'WEAPON_UNARMED' then
+      table.insert(elements, {label = weaponList[i].label, value = weaponList[i].name})
+    end
+
+  end
+
+  ESX.UI.Menu.Open(
+    'default', GetCurrentResourceName(), 'armory_put_weapon',
+    {
+      title    = 'Evidence - Put Weapon',
+      align    = 'top-left',
+      elements = elements
+    },
+    function(data, menu)
+
+      menu.close()
+
+      ESX.TriggerServerCallback('esx_evidence:addArmoryWeapon', function()
+        OpenPutWeaponMenu()
+      end, data.current.value, true)
+
+    end,
+    function(data, menu)
+      menu.close()
+    end
+  )
+
+end
+
+function OpenGetStocksMenu()
+
+  ESX.TriggerServerCallback('esx_evidence:getStockItems', function(items)
+
+
+    local elements = {}
+
+    for i=1, #items, 1 do
+      table.insert(elements, {label = 'x' .. items[i].count .. ' ' .. items[i].label, value = items[i].name})
+    end
+
+    ESX.UI.Menu.Open(
+      'default', GetCurrentResourceName(), 'stocks_menu',
+      {
+        title    = 'Evidence Locker',
+        align    = 'top-left',
+        elements = elements
+      },
+      function(data, menu)
+
+        local itemName = data.current.value
+
+        ESX.UI.Menu.Open(
+          'dialog', GetCurrentResourceName(), 'stocks_menu_get_item_count',
+          {
+            title = _U('quantity')
+          },
+          function(data2, menu2)
+
+            local count = tonumber(data2.value)
+
+            if count == nil then
+              ESX.ShowNotification(_U('quantity_invalid'))
+            else
+              menu2.close()
+              menu.close()
+              TriggerServerEvent('esx_evidence:getStockItem', itemName, count)
+
+              Citizen.Wait(300)
+              OpenGetStocksMenu()
+            end
+
+          end,
+          function(data2, menu2)
+            menu2.close()
+          end
+        )
+
+      end,
+      function(data, menu)
+        menu.close()
+      end
+    )
+
+  end)
+
+end
+
+function OpenPutStocksMenu()
+
+  ESX.TriggerServerCallback('esx_evidence:getPlayerInventory', function(inventory)
+
+    local elements = {}
+
+    for i=1, #inventory.items, 1 do
+
+      local item = inventory.items[i]
+
+      if item.count > 0 then
+        table.insert(elements, {label = item.label .. ' x' .. item.count, type = 'item_standard', value = item.name})
+      end
+--
+
+    end
+	
+		  		PlayerData = ESX.GetPlayerData()
+			for i=1, #PlayerData.accounts, 1 do
+				if PlayerData.accounts[i].name == 'black_money' then
+				  local itemBlack = 1
+				    table.insert(elements, {
+				      label     = PlayerData.accounts[i].label .. ' [ $'.. math.floor(PlayerData.accounts[i].money+0.5) ..' ]',
+				      count     = PlayerData.accounts[i].money,
+				      value     = PlayerData.accounts[i].name,
+				      name      = PlayerData.accounts[i].label,
+					  limit     = PlayerData.accounts[i].limit,
+					  type		= 'item_account',
+				    })
+				end
+			end
+
+    ESX.UI.Menu.Open(
+      'default', GetCurrentResourceName(), 'stocks_menu',
+      {
+        title    = 'Evidence - Put Stock',
+        align    = 'top-left',
+        elements = elements
+      },
+      function(data, menu)
+
+        local itemName = data.current.value
+
+		
+        ESX.UI.Menu.Open(
+          'dialog', GetCurrentResourceName(), 'stocks_menu_put_item_count',
+          {
+            title = _U('quantity')
+          },
+          function(data2, menu2)
+
+            local count = tonumber(data2.value)
+
+			if itemName == "black_money" then
+				TriggerServerEvent('esx_evidence:removeBlack', itemName, count)
+				TriggerServerEvent('esx_evidence:putStockItems', itemName, count)
+			end
+			
+            if count == nil then
+              ESX.ShowNotification(_U('quantity_invalid'))
+            else
+              menu2.close()
+              menu.close()
+              TriggerServerEvent('esx_evidence:putStockItems', itemName, count)
+
+              Citizen.Wait(300)
+              OpenPutStocksMenu()
+            end
+
+          end,
+          function(data2, menu2)
+            menu2.close()
+          end
+        )
+
+      end,
+      function(data, menu)
+        menu.close()
+      end
+    )
+
+  end)
+
+end
+
+RegisterNetEvent('esx:setJob')
+AddEventHandler('esx:setJob', function(job)
+	PlayerData.job = job
+	
+	Citizen.Wait(5000)
+end)
+
+
+
+
+AddEventHandler('esx_evidence:hasEnteredMarker', function(station, part, partNum)
+
+  if part == 'Cloakroom' then
+    CurrentAction     = 'menu_cloakroom'
+    CurrentActionMsg  = _U('open_cloackroom')
+    CurrentActionData = {}
+  end
+
+  if part == 'Armory' then
+    CurrentAction     = 'menu_armory'
+    CurrentActionMsg  = _U('open_armory')
+    CurrentActionData = {station = station}
+  end
+
+  if part == 'VehicleSpawner' then
+    CurrentAction     = 'menu_vehicle_spawner'
+    CurrentActionMsg  = _U('vehicle_spawner')
+    CurrentActionData = {station = station, partNum = partNum}
+  end
+
+  if part == 'HelicopterSpawner' then
+
+    local helicopters = Config.EvidenceLockers[station].Helicopters
+
+    if not IsAnyVehicleNearPoint(helicopters[partNum].SpawnPoint.x, helicopters[partNum].SpawnPoint.y, helicopters[partNum].SpawnPoint.z,  3.0) then
+
+      ESX.Game.SpawnVehicle('polmav', helicopters[partNum].SpawnPoint, helicopters[partNum].Heading, function(vehicle)
+        SetVehicleModKit(vehicle, 0)
+        SetVehicleLivery(vehicle, 0)
+      end)
+
+    end
+
+  end
+
+  if part == 'VehicleDeleter' then
+
+    local playerPed = PlayerPedId()
+    local coords    = GetEntityCoords(playerPed)
+
+    if IsPedInAnyVehicle(playerPed,  false) then
+
+      local vehicle = GetVehiclePedIsIn(playerPed, false)
+
+      if DoesEntityExist(vehicle) then
+        CurrentAction     = 'delete_vehicle'
+        CurrentActionMsg  = _U('store_vehicle')
+        CurrentActionData = {vehicle = vehicle}
+      end
+
+    end
+
+  end
+
+  if part == 'BossActions' then
+    CurrentAction     = 'menu_boss_actions'
+    CurrentActionMsg  = _U('open_bossmenu')
+    CurrentActionData = {}
+  end
+
+end)
+
+AddEventHandler('esx_evidence:hasExitedMarker', function(station, part, partNum)
+  ESX.UI.Menu.CloseAll()
+  CurrentAction = nil
+end)
+
+
+-- Create blips
+-- Citizen.CreateThread(function()
+
+  -- for k,v in pairs(Config.EvidenceLockers) do
+
+    -- local blip = AddBlipForCoord(v.Blip.Pos.x, v.Blip.Pos.y, v.Blip.Pos.z)
+
+    -- SetBlipSprite (blip, v.Blip.Sprite)
+    -- SetBlipDisplay(blip, v.Blip.Display)
+    -- SetBlipScale  (blip, v.Blip.Scale)
+    -- SetBlipColour (blip, v.Blip.Colour)
+    -- SetBlipAsShortRange(blip, true)
+
+    -- BeginTextCommandSetBlipName("STRING")
+    -- AddTextComponentString(_U('map_blip'))
+    -- EndTextCommandSetBlipName(blip)
+
+  -- end
+
+-- end)
+
+-- Display markers
+Citizen.CreateThread(function()
+  while true do
+
+    Wait(0)
+
+    if PlayerData.job ~= nil and PlayerData.job.name == 'police' then
+
+      local playerPed = PlayerPedId()
+      local coords    = GetEntityCoords(playerPed)
+
+      for k,v in pairs(Config.EvidenceLockers) do
+
+        for i=1, #v.Locker, 1 do
+          if GetDistanceBetweenCoords(coords,  v.Locker[i].x,  v.Locker[i].y,  v.Locker[i].z,  true) < Config.DrawDistance then
+            DrawMarker(Config.MarkerType, v.Locker[i].x, v.Locker[i].y, v.Locker[i].z, 0.0, 0.0, 0.0, 0, 0.0, 0.0, Config.MarkerSize.x, Config.MarkerSize.y, Config.MarkerSize.z, Config.MarkerColor.r, Config.MarkerColor.g, Config.MarkerColor.b, 100, false, true, 2, false, false, false, false)
+          end
+        end
+
+        if PlayerData.job ~= nil and PlayerData.job.name == 'police' and PlayerData.job.grade_name == 'boss' or PlayerData.job.grade_name == 'chief' then
+
+          for i=1, #v.BossActions, 1 do
+            if not v.BossActions[i].disabled and GetDistanceBetweenCoords(coords,  v.BossActions[i].x,  v.BossActions[i].y,  v.BossActions[i].z,  true) < Config.DrawDistance then
+              DrawMarker(Config.MarkerType, v.BossActions[i].x, v.BossActions[i].y, v.BossActions[i].z, 0.0, 0.0, 0.0, 0, 0.0, 0.0, Config.MarkerSize.x, Config.MarkerSize.y, Config.MarkerSize.z, Config.MarkerColor.r, Config.MarkerColor.g, Config.MarkerColor.b, 100, false, true, 2, false, false, false, false)
+            end
+          end
+
+        end
+
+      end
+
+    end
+
+  end
+end)
+
+-- Enter / Exit marker events
+Citizen.CreateThread(function()
+
+  while true do
+
+    Wait(0)
+
+    if PlayerData.job ~= nil and PlayerData.job.name == 'police' then
+
+      local playerPed      = PlayerPedId()
+      local coords         = GetEntityCoords(playerPed)
+      local isInMarker     = false
+      local currentStation = nil
+      local currentPart    = nil
+      local currentPartNum = nil
+
+      for k,v in pairs(Config.EvidenceLockers) do
+
+        for i=1, #v.Locker, 1 do
+          if GetDistanceBetweenCoords(coords,  v.Locker[i].x,  v.Locker[i].y,  v.Locker[i].z,  true) < Config.MarkerSize.x then
+            isInMarker     = true
+            currentStation = k
+            currentPart    = 'Armory'
+            currentPartNum = i
+          end
+        end
+
+
+        if PlayerData.job ~= nil and PlayerData.job.name == 'police' and PlayerData.job.grade_name == 'boss' or PlayerData.job.grade_name == 'chief' then
+
+          for i=1, #v.BossActions, 1 do
+            if GetDistanceBetweenCoords(coords,  v.BossActions[i].x,  v.BossActions[i].y,  v.BossActions[i].z,  true) < Config.MarkerSize.x then
+              isInMarker     = true
+              currentStation = k
+              currentPart    = 'BossActions'
+              currentPartNum = i
+            end
+          end
+
+        end
+
+      end
+
+      local hasExited = false
+
+      if isInMarker and not HasAlreadyEnteredMarker or (isInMarker and (LastStation ~= currentStation or LastPart ~= currentPart or LastPartNum ~= currentPartNum) ) then
+
+        if
+          (LastStation ~= nil and LastPart ~= nil and LastPartNum ~= nil) and
+          (LastStation ~= currentStation or LastPart ~= currentPart or LastPartNum ~= currentPartNum)
+        then
+          TriggerEvent('esx_evidence:hasExitedMarker', LastStation, LastPart, LastPartNum)
+          hasExited = true
+        end
+
+        HasAlreadyEnteredMarker = true
+        LastStation             = currentStation
+        LastPart                = currentPart
+        LastPartNum             = currentPartNum
+
+        TriggerEvent('esx_evidence:hasEnteredMarker', currentStation, currentPart, currentPartNum)
+      end
+
+      if not hasExited and not isInMarker and HasAlreadyEnteredMarker then
+
+        HasAlreadyEnteredMarker = false
+
+        TriggerEvent('esx_evidence:hasExitedMarker', LastStation, LastPart, LastPartNum)
+      end
+
+    end
+
+  end
+end)
+
+
+-- Key Controls
+Citizen.CreateThread(function()
+	while true do
+
+		Citizen.Wait(0)
+
+		if CurrentAction ~= nil then
+			SetTextComponentFormat('STRING')
+			AddTextComponentString(CurrentActionMsg)
+			DisplayHelpTextFromStringLabel(0, 0, 1, -1)
+
+			if IsControlJustReleased(0, Keys['E']) and PlayerData.job ~= nil and PlayerData.job.name == 'police' then
+
+				if CurrentAction == 'menu_cloakroom' then
+					OpenCloakroomMenu()
+				elseif CurrentAction == 'menu_armory' then
+
+						-- OpenArmoryMenu(CurrentActionData.station)
+						OpenArmoryMenu()
+
+				elseif CurrentAction == 'menu_vehicle_spawner' then
+					OpenVehicleSpawnerMenu(CurrentActionData.station, CurrentActionData.partNum)
+				elseif CurrentAction == 'delete_vehicle' then
+					if Config.EnableSocietyOwnedVehicles then
+						local vehicleProps = ESX.Game.GetVehicleProperties(CurrentActionData.vehicle)
+						TriggerServerEvent('esx_society:putVehicleInGarage', 'police', vehicleProps)
+					end
+					ESX.Game.DeleteVehicle(CurrentActionData.vehicle)
+				elseif CurrentAction == 'menu_boss_actions' then
+					ESX.UI.Menu.CloseAll()
+					TriggerEvent('esx_society:openBossMenu', 'police', function(data, menu)
+						menu.close()
+						CurrentAction     = 'menu_boss_actions'
+						CurrentActionMsg  = _U('open_bossmenu')
+						CurrentActionData = {}
+					end, { wash = false }) -- disable washing money
+				elseif CurrentAction == 'remove_entity' then
+					DeleteEntity(CurrentActionData.entity)
+				end
+				
+				CurrentAction = nil
+			end
+		end -- CurrentAction end
+	end
+end)
+
+
+AddEventHandler('playerSpawned', function(spawn)
+	isDead = false
+	
+	if not hasAlreadyJoined then
+		TriggerServerEvent('esx_evidence:spawned')
+	end
+	hasAlreadyJoined = true
+end)
+
+AddEventHandler('esx:onPlayerDeath', function(data)
+	isDead = true
+end)
+
+AddEventHandler('onResourceStop', function(resource)
+	if resource == GetCurrentResourceName() then
+
+	end
+end)
+
+
+function GetCharacterName(source)
+	-- fetch identity in sync
+	local result = MySQL.Sync.fetchAll('SELECT * FROM users WHERE identifier = @identifier',
+	{
+		['@identifier'] = GetPlayerIdentifiers(source)[1]
+	})
+
+	if result[1] ~= nil and result[1].firstname ~= nil and result[1].lastname ~= nil then
+		if Config.OnlyFirstname then
+			return result[1].firstname
+		else
+			return result[1].firstname .. ' ' .. result[1].lastname
+		end
+	else
+		return GetPlayerName(source)
+	end
+end
