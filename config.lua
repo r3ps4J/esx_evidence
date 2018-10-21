0f4c56d80b3c83e494abaa9a57e7087b877dda39
xnubil <39658424+xnubil@users.noreply.github.com>
2018-10-21 10:29:27 -0400

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,23 @@
+Config                            = {}
+
+Config.DrawDistance               = 100.0
+Config.MarkerType                 = 23
+Config.MarkerSize                 = { x = 1.5, y = 1.5, z = 1.0 }
+Config.MarkerColor                = { r = 204, g = 50, b = 50 }
+
+
+Config.Locale = 'en'
+
+
+LSPD = {
+
+		Locker = {
+			{x = 455.84, y = -979.65, z = 29.69},	--Mission Row
+			{x = 1851.28, y = 3683.54, z = 33.31},	--Sandy Shores
+			{x = -442.79, y = 6012.56, z = 30.79},	--Poleto Bay
+		},
+
+	}
+}
+
+
